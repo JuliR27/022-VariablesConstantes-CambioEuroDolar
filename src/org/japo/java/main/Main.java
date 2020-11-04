ee2e2cb63f1218c8853d087cbdd563fa016fe361
Julian David Ramos Gomez <juli12319@hotmail.com>
2020-11-04 21:15:07 +0100

    Euro Dolar Constante

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author CicloM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        final double Cambio_Euro_Dolar = 0.85;
+        System.out.printf("El Cambio de Hoy es 1£ por %.2f%n", Cambio_Euro_Dolar);
+    }
+
+}
